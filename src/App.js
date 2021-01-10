81bbca9896512323da63d4f56a9a8d7329f7c276
Mike Moran <mike@houseofmoran.com>
2021-01-10 13:27:26 +0000

    add bulma

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React, { useContext, useState } from "react";
-import './App.css';
-import 'react-calendar/dist/Calendar.css';
+import './App.scss';
 
-import Calendar from 'react-calendar';
 
 import { observer } from "mobx-react-lite";
 import { StoreProvider, StoreContext, Store } from "./model/contexts";
 
 const Root = observer(() => {
   const { store } = useContext(StoreContext);
-  const [value, setValue] = useState(new Date());
-
-  function onChange(nextValue) {
-    setValue(nextValue);
-  }
 
   return (
     <div className="App">
-      <button onClick={() => store.addTaggedDate("some_tag")}>Click Me</button>
       <ul>
       {store.tagged.map((taggedDate) => {
         return <li key={taggedDate.tag}>{taggedDate.tag}</li>
       })}
       </ul>
-      <Calendar
-        onChange={onChange}
-        value={value}
-      />
+      <button className="button" onClick={() => store.addTaggedDate("some_tag")}>Click Me</button>
     </div>
   );
 });
