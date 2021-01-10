cacccd203ef2b498bc2bf145da5e2e538210e9bd
Mike Moran <mike@houseofmoran.com>
2021-01-10 00:09:53 +0000

    add calendar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './App.css';
+import 'react-calendar/dist/Calendar.css';
+
+import Calendar from 'react-calendar';
 
 import { observer } from "mobx-react-lite";
 import { StoreProvider, StoreContext, Store } from "./model/contexts";
 
 const Root = observer(() => {
   const { store } = useContext(StoreContext);
+  const [value, setValue] = useState(new Date());
+
+  function onChange(nextValue) {
+    setValue(nextValue);
+  }
+
   return (
     <div className="App">
       <button onClick={() => store.addTaggedDate("some_tag")}>Click Me</button>
@@ -14,6 +23,10 @@ const Root = observer(() => {
         return <li key={taggedDate.tag}>{taggedDate.tag}</li>
       })}
       </ul>
+      <Calendar
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 });
